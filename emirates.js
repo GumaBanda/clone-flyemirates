f767397f5edee6482a98c0e122b66a2cd2788b6b
guma144 <gumanbanda144@gmail.com>
2022-07-24 23:57:03 +0100

    second commit

---
+++ emirates.js
@@ -0,0 +1,10 @@
+// $("li").text("HAIL JESUS");
+// $("a").attr("href", "https://www.instagram.com/zacretz/");
+// $("li").click(function() {
+//     $("li").css("background-color", "green");
+// });
+
+
+// $("body").keypress(function(event) {
+//     $("p").text(event.key);
+// });
